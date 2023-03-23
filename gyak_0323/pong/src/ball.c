0dc6afaa555230d572aa2afa895b7ded5dc951f2
kgorog989 <kgorog989@gmail.com>
2023-03-23 12:49:23 +0100

    pong game

---
+++ gyak_0323/pong/src/ball.c
@@ -0,0 +1,42 @@
+#include "ball.h"
+
+#include <GL/gl.h>
+
+#include <math.h>
+
+void init_ball(Ball* ball, float x, float y)
+{
+    ball->x = x;
+    ball->y = y;
+    ball->radius = 25;
+    ball->speed_x = 200;
+    ball->speed_y = 200;
+}
+
+void update_ball(Ball* ball, double time)
+{
+    ball->x += ball->speed_x * time;
+    ball->y += ball->speed_y * time;
+}
+
+void render_ball(Ball* ball)
+{
+    double angle;
+    double x;
+    double y;
+
+    glPushMatrix();
+    glTranslatef(ball->x, ball->y, 0.0);
+    glBegin(GL_TRIANGLE_FAN);
+    glColor3f(0.7, 0.5, 0.8);
+    glVertex2f(0, 0);
+    angle = 0;
+    while (angle < 2.0 * M_PI + 1) {
+        x = cos(angle) * ball->radius;
+        y = sin(angle) * ball->radius;
+        glVertex2f(x, y);
+        angle += 0.1;
+    }
+    glEnd();
+    glPopMatrix();
+}
